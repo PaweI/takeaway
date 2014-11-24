904802370c368c92de4b1e9517396797a7c9ec12
Pavel Redics <pavel.redics@gmail.com>
2014-11-24 14:03:02 +0000

    Order

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,29 @@
 class Order
 
+  attr_reader :dishes
+
+  def initialize
+    @dishes = []
+  end
+
+  def add(dish)
+    dishes << dish
+  end
+
+  def cost(dish)
+    dish.price
+  end
+
+  def total_to_pay(result = 0)
+    dishes.each do |dish| 
+      result += dish[:price]
+    end
+    result
+  end
+
+
+
+
+
+
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,4 +2,24 @@ require 'order'
 
 describe Order do
 
+  let(:order) { Order.new    }
+  let(:dish)  { double :dish, price: 5 } 
+
+  it 'should list all dishes in order' do
+    order.add(dish)
+    expect(order.dishes).to eq [dish]
+  end
+
+  it 'should show their prices' do
+    expect(order.cost(dish)).to eq 5    
+  end
+
+  it 'should show total amount to pay' do
+    dish = { price: 5 }
+    dish2 = { price: 10 }
+    order.add(dish)
+    order.add(dish2)
+    expect(order.total_to_pay).to eq 15
+  end
+
 end
