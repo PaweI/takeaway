f47df74bc9eff133fd3f73f7bb69ab008d5a0baa
Pavel Redics <pavel.redics@gmail.com>
2014-11-24 13:43:15 +0000

    Menu class

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,20 @@
 class Menu
 
+  attr_reader :dishes
+
+  def initialize
+    @dishes = []
+  end
+
+  def add(dish)
+    dishes << dish
+  end
+
+  def cost(dish)
+    dish.price
+  end
+
+  def describe(dish)
+    dish.description
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,4 +1,24 @@
 require 'menu'
 
 describe Menu do
+
+  let(:menu) { Menu.new }
+  let(:dish) { double name: "dish", price: 5 }
+  let(:dish2) { double name: "dish2", price: 10 }
+
+  it 'should list all the dishes available by name' do
+    menu.add(dish)
+    menu.add(dish2)
+    expect(menu.dishes).to eq([dish, dish2])
+  end
+
+  it 'should show price of the dish' do
+    menu.add(dish)
+    expect(menu.cost(dish)).to eq 5
+  end
+
+  it 'should show dish description' do
+    expect(dish).to receive(:description)
+    menu.describe(dish)
+  end
 end
