80d4ce36f69007f1953f1ef580846c1eefaf7604
Pavel Redics <pavel.redics@gmail.com>
2014-11-24 11:59:43 +0000

    Sends message

--- lib/message.rb
+++ lib/message.rb
@@ -1,15 +1,17 @@
 require 'twilio-ruby'
 
 class Message
- 
-  @account_sid = "ACb49a63dc4878f057a6a903714afa158c"
-  @auth_token = ENV['TOCKEN']
-  
-  @client = Twilio::REST::Client.new @account_sid, @auth_token
 
-  def send_message_to_client(text)
-    @message = @client.account.messages.create({:to => "+447969959235", 
+  account_sid = ENV['TWILIO_ID']
+  auth_token = ENV['TWILIO_TOCKEN']
+  CLIENT = Twilio::REST::Client.new account_sid, auth_token
+ 
+  DELIVERY_TIME = (Time.now.hour+1).to_s + ":" + Time.now.min.to_s
+
+  def send_message_to_client(number)
+    @message = CLIENT.account.messages.create({:to => number, 
       :from => "+441212853508", 
-      :body => text})
+      :body => "Thank you! Your order was placed and will be delivered before #{DELIVERY_TIME}"})
+    "Message has been sent"
   end
 end
