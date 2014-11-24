1a5870eb8b9ccdeae0799d0ca6bbf1b4a776cc0b
Pavel Redics <pavel.redics@gmail.com>
2014-11-24 15:17:04 +0000

    Customer

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,14 @@
 class Customer
 
+  def look_at(menu)
+    menu.dishes
+  end
+
+  def choose(dish, order)
+    order.add(dish)
+  end
+
+  def pay(order)
+    order.receive_money
+  end
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -2,4 +2,23 @@ require 'customer'
 
 describe Customer do
 
+  let(:customer) { Customer.new                 }
+  let(:menu)     { double :menu, dishes: "dish" }
+  let(:order)    { double :order                }
+  let(:dish)     { double :dish                 }
+
+  it 'be able to see all items in the menu' do
+    expect(customer.look_at(menu)).to eq "dish"
+  end
+
+  it 'be able to choose dishes from the menu and put the into order' do
+    expect(order).to receive(:add)
+    customer.choose(dish, order)
+  end
+
+  it 'be able to pay for order' do
+    expect(order).to receive(:receive_money)
+    customer.pay(order)
+  end
+
 end
