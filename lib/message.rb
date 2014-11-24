19cbd56617cdccc93bb0204b1b2aa8a14fb745cc
Pavel Redics <pavel.redics@gmail.com>
2014-11-24 12:19:01 +0000

    change Time.now.min to .strftime

--- lib/message.rb
+++ lib/message.rb
@@ -6,7 +6,7 @@ class Message
   auth_token = ENV['TWILIO_TOCKEN']
   CLIENT = Twilio::REST::Client.new account_sid, auth_token
  
-  DELIVERY_TIME = (Time.now.hour+1).to_s + ":" + Time.now.min.to_s
+  DELIVERY_TIME = (Time.now.hour+1).to_s + ":" + (Time.now.strftime "%M")
 
   def send_message_to_client(number)
     @message = CLIENT.account.messages.create({:to => number, 
@@ -15,3 +15,4 @@ class Message
     "Message has been sent"
   end
 end
+
